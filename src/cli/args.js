b0cb9a1801800f76640c5ee667516ca7e33f46a8
StanislavDevel <unfeel00@gmail.com>
2025-10-26 23:28:08 +0100

    feat:add solution args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv.slice(2);
+  const arr = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].startsWith('--') ? args[i].slice(2) : args[i];
+    const value = args[i + 1];
+    arr.push(`${key} is ${value}`);
+  }
+  console.log(arr.join(', '));
 };
 
 parseArgs();
